900eb8561f770c7e068b887b22bd1c1c664d969b
daniilseliverstov <selivrstov91@internet.ru>
2024-10-27 15:31:15 +0300

    Urls и Views. Добавление папки

---
+++ UrbanDjango/task2/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def class_temp(request):
+    return render(request, 'second_task/class_template.html')
+
+
+def func_temp(request):
+    return render(request, 'second_task/func_template.html')
+
+
+def m_p(request):
+    return HttpResponse('Главная Страница')
